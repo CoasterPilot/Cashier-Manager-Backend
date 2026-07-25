2b5677eb3bfd7b0560aaf35110e35ce078aab010
Timo Fischer <timo.rocky@gmail.com>
2026-07-26 01:19:02 +0200

    Change API for Client

--- flask_server.py
+++ flask_server.py
@@ -10,7 +10,9 @@ from database_connection import create_connection
 
 app = Flask(__name__)
 
+
 @app.route('/accounts', methods=['POST'])
+#Get all accounts
 def get_accounts():
     TOKEN = read_config_value("config.txt", "API_Token")
     auth_header = request.headers.get("Authorization", "")
@@ -28,7 +30,7 @@ def get_accounts():
         accounts = cursor.fetchall()
         #Ausgabe für den Server zum Debuggen. Ausgabe wird an Server geschickt, damit der Client die Daten erhält.
         print(f"Accounts retrieved successfully: {accounts}")
-        return jsonify({"message": f"Accounts retrieved successfully, accounts: {accounts}", "accounts": accounts})
+        return jsonify({"message": f"Accounts retrieved successfully", "accounts": accounts})
     
     except Exception as e:
         print(f"Error retrieving accounts: {e}")
@@ -40,6 +42,7 @@ def get_accounts():
             conn.close()
 
 @app.route('/get_account_data', methods=['POST'])
+#Get Account Data
 def get_account_data():
     TOKEN = read_config_value("config.txt", "API_Token")
     auth_header = request.headers.get("Authorization", "")
@@ -125,6 +128,60 @@ def update_balance():
             cursor.close()
         if conn:
             conn.close()
+
+
+@app.route('/add_new_food', methods=['POST'])
+def add_new_food():
+    TOKEN = read_config_value("config.txt", "API_Token")
+    auth_header = request.headers.get("Authorization", "")
+    if not auth_header.startswith("Bearer "):
+        return abort(401, "Authentifizierung erforderlich.")
+
+    token_received = auth_header.split("Bearer ")[1].strip()
+    if token_received != TOKEN:
+        return abort(401, "Falsches Token")
+    
+
+    try:
+        paylist = []
+        data = request.get_json()
+        username = data["username"]
+        person_and_price_list = data["person_and_price_list"]
+        for transaction in person_and_price_list:
+            print(f"Received transaction: {transaction}")
+            price = transaction["price"]
+            name = transaction["paid_by"]
+            paylist.append((name, float(price), username))
+
+        if not name or price is None:
+            return abort(400, "Name und Price sind erforderlich.")
+
+        conn = create_connection()
+        cursor = conn.cursor()
+        for transaction in paylist:
+            name = transaction[0]
+            price = transaction[1]
+            username = transaction[2]
+            cursor.execute("SELECT cash_balance FROM Accounts WHERE username = %s", (name,))
+            old_balance = cursor.fetchone()[0]
+            new_price = old_balance + price
+
+            # Update the cash balance in the Accounts table
+            cursor.execute("UPDATE Accounts SET cash_balance = %s WHERE username = %s", (new_price, name))
+            # Insert into a History table for tracking food purchases
+            cursor.execute("INSERT INTO BalanceHistory (username, reason, old_balance, change_amount, new_balance, date, amount_from) VALUES (%s, %s, %s, %s, %s, %s, %s)", (name, "Food Purchase", old_balance, price, new_price, datetime.datetime.now(), username))
+        conn.commit()
+        print(f"New food item added successfully: {name} with price {price}")
+        return jsonify({"message": f"New food item added successfully: {name} with price {price}"})
+    except Exception as e:
+        print(f"Error adding new food item: {e}")
+        return jsonify({"error": str(e)}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+             conn.close()
+
 
 if __name__ == "__main__":
     host = read_config_value("config.txt", "Server_Host")
